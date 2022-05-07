f9c2f081b2df5b898102c3e23a871f9d111525e2
Dinesh Sharma <93374756+dinoweblog@users.noreply.github.com>
2022-05-07 23:45:15 +0530

    Update store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -4,22 +4,22 @@ import thunk from "redux-thunk";
 import { cityReducer } from "./City/reducer";
 import { countryReducer } from "./Country/reducer";
 
-const composeEnhancers =
-  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-      })
-    : compose;
+// const composeEnhancers =
+//   typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+//         // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+//       })
+//     : compose;
 
 const middleware = [thunk];
 
-const enhancer = composeEnhancers(
-  applyMiddleware(...middleware)
-  // other store enhancers if any
-);
+// const enhancer = composeEnhancers(
+//   applyMiddleware(...middleware)
+//   // other store enhancers if any
+// );
 const rootReducer = combineReducers({
   city: cityReducer,
   country: countryReducer,
 });
 
-export const store = createStore(rootReducer, enhancer);
+export const store = createStore(rootReducer,  applyMiddleware(...middleware));
